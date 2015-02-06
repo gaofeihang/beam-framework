0d754c1e485f7c6c61a7083d6a886f15cce5a5e6
gaofeihang <gaofeihang@163.com>
2015-02-06 12:01:32 +0800

    ThreadUtils

---
+++ beam-commons/src/main/java/net/beamlight/commons/util/ThreadUtils.java
@@ -0,0 +1,17 @@
+package net.beamlight.commons.util;
+
+/**
+ * @author gaofeihang
+ * @since Feb 5, 2015
+ */
+public class ThreadUtils {
+    
+    public static void sleep(long millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
